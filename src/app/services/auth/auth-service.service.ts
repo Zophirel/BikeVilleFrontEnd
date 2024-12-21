3b875210c5d34b24077f0ec8cbc86f753e9bf01f
Francesco <sworld.design@gmail.com>
2024-12-21 13:28:46 +0100

    added google oauth

--- src/app/services/auth/auth-service.service.ts
+++ src/app/services/auth/auth-service.service.ts
@@ -8,10 +8,27 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   auth : string | null = null;
+  private clientId = '921059556192-2brjfar3rbda76hnb54k7djojj4ccdg7.apps.googleusercontent.com';
   client: HttpClient;
   
   constructor(client: HttpClient) {
     this.client = client;
+  }
+
+  public initializeGoogleSignIn(callback: (response: any) => void): void {
+    (window as any).google.accounts.id.initialize({
+      client_id: this.clientId,
+      callback: callback,
+    });
+
+    (window as any).google.accounts.id.renderButton(
+      document.getElementById('google-button'),
+      { theme: 'outline', size: 'large' }
+    );
+  }
+
+  public promptGoogleOneTap(): void {
+    (window as any).google.accounts.id.prompt();
   }
 
   login(email: string, password: string) : Observable<any> {
