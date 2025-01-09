1d26ffde146d79770b8edfc79b41bebf47922e41
Francesco <sworld.design@gmail.com>
2025-01-09 17:19:39 +0100

    finished authentication processes

--- src/app/account/account/account.component.ts
+++ src/app/account/account/account.component.ts
@@ -1,20 +1,22 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthGoogleService } from '../../services/auth/google.service';
 
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [NavbarComponent, CommonModule, RouterModule],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
 
 export class AccountComponent {
+  private googleService = inject(AuthGoogleService);
+  private router = inject(Router);  
   isDropdownOpen = false; // Stato che indica se il dropdown è aperto o chiuso
-
   images: string[] = [
     "https://bikeville.s3.cubbit.eu/images/account/greater-than.png",
     "https://bikeville.s3.cubbit.eu/images/account/down.png",
@@ -30,6 +32,11 @@ export class AccountComponent {
     }else{
       this.currentArrow = this.images[0];
     }
+  }
+
+  logout() {
+    this.googleService.logout();
+    this.router.navigateByUrl('/home');
   }
 
   // Ascolta i clic fuori dal menu per chiuderlo
--- src/app/services/auth/oauth-config.ts
+++ src/app/services/auth/oauth-config.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
-  redirectUri: `${window.location.origin}${window.location.pathname}`,
+  redirectUri: `${window.location.href}`,
   clientId: '921059556192-2brjfar3rbda76hnb54k7djojj4ccdg7.apps.googleusercontent.com',
   scope: 'openid profile email address phone',
   strictDiscoveryDocumentValidation: false
