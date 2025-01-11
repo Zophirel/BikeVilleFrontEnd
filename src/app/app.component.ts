6ea98ed1ef8599a28da6d590f57520207a06dc04
Francesco <sworld.design@gmail.com>
2025-01-11 17:47:30 +0100

    added page transition animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { trigger, transition, style, animate } from '@angular/animations'
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet, CommonModule],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    animations: [
+      trigger('routeAnimations', [
+        transition('* <=> *', [
+          style({ opacity: 0 }),
+          animate('500ms', style({ opacity: 1 })),
+        ]),
+      ]),
+    ],
 })
 export class AppComponent { 
   title = 'BikeVille';
+  prepareRoute(outlet: any) {
+    return outlet?.activatedRouteData?.['animation'];
+  }
 }
