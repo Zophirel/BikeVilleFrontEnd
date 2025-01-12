067f69e209105381c8684fb0fce70baad95737a7
delia99 <deliaricca99@gmail.com>
2025-01-12 19:17:31 +0100

    cart and checkout order management integration

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -3,6 +3,8 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../services/cart/cart.service';
+import { AddressService } from '../services/address/address.service';
+import { OrderShipping } from '../models/order-shipping.model';
 
 @Component({
   selector: 'checkout',
@@ -14,11 +16,29 @@ import { CartService } from '../services/cart/cart.service';
 export class CheckoutComponent implements OnInit {
   quantity: number = 1;
   total: number = 0;
+  address: OrderShipping | undefined;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private addressService: AddressService) {}
 
   ngOnInit() {
-    this.quantity = this.cartService.getQuantity();
+    this.quantity = this.cartService.getNumberOfItems();
     this.total = this.cartService.getTotal();
+
+    const shippingAddressId = this.cartService.getShippingAddressId();
+
+    if(shippingAddressId){
+      this.loadShippingAddress(shippingAddressId);
+    }
+  }
+
+  loadShippingAddress(orderId: string) {
+    this.addressService.getAddress(orderId).subscribe({
+      next: (address) => {
+        this.address = address; 
+      },
+      error: (error) => {
+        console.error('Error loading shipping address:', error);
+      }
+    });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     LOGIN_URL: 'https://zophirel.it/api/auth/login',
     SIGNUP_URL: 'https://zophirel.it/api/auth/signup',
     FORGOT_PASS_URL: 'https://zophirel.it/api/auth/forgot-password',
+    BASE_URL: 'https://zophirel.it'
 };
